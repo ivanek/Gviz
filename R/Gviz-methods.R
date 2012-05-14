dee1957f537e851862abcb1f9e70f25fa92c1f8c
Florian Hahne <f.hahne@dkfz-heidelberg.de>
2012-05-14 14:39:55 +0000

    adding missing elements to Namespace
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Gviz@65871 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/Gviz-methods.R
+++ R/Gviz-methods.R
@@ -1876,7 +1876,7 @@ setMethod("drawGD", signature("DataTrack"), function(GdObject, minBase, maxBase,
                              ##panel.lines(unit(c(0,boxSize), "inches"), c(0.5, 0.5), col=pcols$col.line[i], lwd=pcols$lwd[i], lty=pcols$lty[i])
                              grid.lines(unit(c(0,boxSize), "inches"), c(0.5, 0.5), gp=gpar(col=pcols$col.line[i], lwd=pcols$lwd[i], lty=pcols$lty[i]))
                      }
-                     grid.text(x=unit(boxSize+spacing, "inches"), y=0.5, adj=c(0, 0.5), label=grpLevels[i], gp=gpar(col=fontcolor))
+                     grid.text(x=unit(boxSize+spacing, "inches"), y=0.5, just=c(0, 0.5), label=grpLevels[i], gp=gpar(col=fontcolor))
                      popViewport(1)
                  }
                  popViewport(2)
