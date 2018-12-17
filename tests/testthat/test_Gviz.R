e65bc73d1e1fad785deba633c1b0252b587e2f73
Robert Ivánek <robert.ivanek@unibas.ch>
2018-12-17 14:12:57 +0100

    added tests for devRes, .getImageMap

--- tests/testthat/test_Gviz.R
+++ tests/testthat/test_Gviz.R
@@ -1,5 +1,7 @@
 library(Gviz)
 
+context("Gviz functions")
+
 test_that("checking the class and structure works", {
   expect_null(Gviz:::.checkClass(data.frame(), "data.frame"))
   expect_error(Gviz:::.checkClass(data.frame(), "matrix"), "of class")
@@ -16,3 +18,26 @@ test_that("conversion of chromosome names works", {
   options("ucscChromosomeNames" = FALSE)
   expect_identical(Gviz:::.chrName(c("1", "chr1", "M", "X")), c("1","chr1","M","X"))
 })
+
+## not sure how to set up the test for the device resolution
+## with opening PDF device?
+test_that("estimating of device resolution works", {
+  #if (file.exists("Rplots.pdf")) unlink("Rplots.pdf")
+  #pdf(file="Rplots.pdf", width=7, height=5, pointsize=12)
+  expect_identical(Gviz:::devRes(), c(xres=72, yres=72))
+  #dev.off()
+  #unlink("Rplots.pdf")
+})
+
+test_that("estimating of coordinates for an HTML image map works", {
+  m <- matrix(rep(0:1, each=4), ncol=4, byrow=TRUE)
+  mm <- matrix(rep(as.numeric(c(0,1,504,503)), 2), ncol=4,
+               dimnames=list(NULL, c("x1","y1","x2","y2")))
+  mm <- as.data.frame(mm)
+  # if (file.exists("Rplots.pdf")) unlink("Rplots.pdf")
+  # pdf(file="Rplots.pdf", width=7, height=5, pointsize=12)
+  expect_identical(Gviz:::.getImageMap(m), mm)
+  # dev.off()
+  # unlink("Rplots.pdf")
+})
+
