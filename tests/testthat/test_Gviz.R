9629041d490482624cc80bb1e548effaf2d4b8aa
Robert Ivánek <robert.ivanek@unibas.ch>
2018-12-17 14:52:39 +0100

    refined tests for devRes, .getImageMap, vpLocation

--- tests/testthat/test_Gviz.R
+++ tests/testthat/test_Gviz.R
@@ -21,10 +21,31 @@ test_that("conversion of chromosome names works", {
 
 ## not sure how to set up the test for the device resolution
 ## with opening PDF device?
+test_that("finding location and pixel-size of current viewport works", {
+  l <- list(location = c(x1 = 0, y1 = 0, x2 = 504, y2 = 504), 
+            size = c(width = 504, height = 504), 
+            ilocation = c(x1 = 0, y1 = 0, x2 = 7, y2 = 7), 
+            isize = c(width = 7, height = 7))
+  #if (file.exists("Rplots.pdf")) unlink("Rplots.pdf")
+  #pdf(file="Rplots.pdf", width=7, height=5, pointsize=12)
+  expect_identical(Gviz:::vpLocation(), l)
+  #dev.off()
+  #unlink("Rplots.pdf")
+})
+
+
 test_that("estimating of device resolution works", {
   #if (file.exists("Rplots.pdf")) unlink("Rplots.pdf")
   #pdf(file="Rplots.pdf", width=7, height=5, pointsize=12)
   expect_identical(Gviz:::devRes(), c(xres=72, yres=72))
+  #dev.off()
+  #unlink("Rplots.pdf")
+  
+  #if (file.exists("Rplots.pdf")) unlink("Rplots.pdf")
+  #pdf(file="Rplots.pdf", width=7, height=5, pointsize=12)
+  pushViewport(viewport())
+  expect_identical(Gviz:::devRes(), c(xres=72, yres=72))
+  popViewport()
   #dev.off()
   #unlink("Rplots.pdf")
 })
