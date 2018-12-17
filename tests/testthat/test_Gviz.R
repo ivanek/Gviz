a0520bde326699582c63057d0f906d4ce04d3794
Robert Ivánek <robert.ivanek@unibas.ch>
2018-12-17 11:04:58 +0100

    added first test for Gviz.R

--- tests/testthat/test_Gviz.R
+++ tests/testthat/test_Gviz.R
@@ -3,5 +3,7 @@ library(Gviz)
 test_that("checking the class and structure works", {
     expect_null(Gviz:::.checkClass(data.frame(), "data.frame"))
     expect_error(Gviz:::.checkClass(data.frame(), "matrix"), "of class")
+    expect_error(Gviz:::.checkClass(class="matrix"), "is missing with no default")
+    expect_error(Gviz:::.checkClass(data.frame(), "data.frame", length=2), "of length")
 }
 )
